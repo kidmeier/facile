d1ffa386a092bf5e26f793bdebbb86b029c9fd2c
Michael Reid <kid.meier@gmail.com>
2009-06-19 14:03:01 -0400

    * example.clj: Updated example to accommodate recent changes

--- etc/example.clj
+++ etc/example.clj
@@ -3,28 +3,39 @@
 	clj.facile.el
 	clj.facile.taglib.core))
 
-(defview controls
+(defsession input-session (ref "Type some text"))
+(def input-val (ref "Type some text"))
+(def select-boolean-val (ref nil)) 
+(def select-one-val (ref nil))
+(def select-many-val (ref []))
+
+(defview core
   (output "output-tst" "An example output control.")
   (form "example-form" 
-	(input "name" "Input control")
-	(select-boolean "select-boolean" false)
-;	(select-one "example-select-one"
-;		    "Value 0"
-;		    (select-item "Item 0" "Value 0")
-;		    (select-item "Item 1" "Value 1")
-;		    (select-item "Item 2" "Value 2")
-;		    (select-item "..." "..."))
-;	(select-many "example-select-many"
-;		     0
-;		     (select-item "Item 0" "Value 0")
-;		     (select-item "Item 1" "Value 1")
-;		     (select-item "Item 2" "Value 2")
-;		     (select-item "..." "..."))
-	
+	(input "name" #'input-session)
+	(select-boolean "select-boolean" #'select-boolean-val)
+	(select-one "example-select-one"
+		    #'select-one-val
+		    (select-items {"Item 0" "Value 0"
+				   "Item 1" "Value 1"
+				   "Item 2" "Value 2"
+				   "..." "..."}))
+	(select-many "example-select-many"
+		     #'select-many-val
+		     (select-items {"Item 0" "Value 0"
+				    "Item 1" "Value 1"
+				    "Item 2" "Value 2"
+				    "..." "..."}))
+	  
 	(command "example-cmd-button" 
 		 "Command button"
 		 (fn []
-		   (println "You clicked the button!")))))
+		   (println "State:"
+			    "\n\tinput:\t" @input-session
+			    "\n\tselect-boolean:\t" @select-boolean-val
+			    "\n\tselect-one:\t" @select-one-val
+			    "\n\tselect-many:\t" @select-many-val
+			    "\n")))))
 
 (comment
 	(data "employees" 
